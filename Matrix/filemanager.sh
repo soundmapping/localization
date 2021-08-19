169913bfbf2d8ee7c378a1d5b782a5db96a88763
soundmapping <86074436+soundmapping@users.noreply.github.com>
2021-08-19 06:53:41 -0700

    Manage RAW data

--- Matrix/filemanager.sh
+++ Matrix/filemanager.sh
@@ -3,3 +3,4 @@ find /home/pi/odas/recordings/SSL -mmin +15 -exec rm {} \;
 find /home/pi/odas/recordings/SST -mmin +15 -exec rm {} \;
 find /home/pi/odas/recordings/separated -mmin +15 -exec rm {} \;
 find /home/pi/odas/recordings/postfiltered -mmin +15 -exec rm {} \;
+find /home/pi/odas/recordings/pureRaw -mmin +15 -exec rm {} \;
