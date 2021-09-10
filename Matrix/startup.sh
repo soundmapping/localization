79a3f384e41b43f9eaa43f79f129a48bca92dd65
Soundmapping <soundmapping@ucsd.edu>
2021-09-09 18:37:58 -0700

    Changed startup for offline odaslive

--- Matrix/startup.sh
+++ Matrix/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo -e $(python3 -u /home/pi/odas/python/recordingv4.py &) >> /home/pi/recordingLogs.txt
+# echo -e $(python3 -u /home/pi/odas/python/recordingv4.py &) >> /home/pi/recordingLogs.txt
 # python3 -u /home/pi/odas/python/recording.v3.py &
 # python3 -u /home/pi/odas/python/watchdog.v1.py &
+
+python3 -u /home/pi/odas/python/arecording.py &
+python3 -u /home/pi/odas/python/odasPush.py &
