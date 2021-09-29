690580fb89b128d2be28dd56a8d60032c714b0b6
manuel hahmann <manha@elektro.dtu.dk>
2021-09-29 15:19:14 -0700

    dummy path for separated/postfiltered

--- Matrix/python/odasOffline.py
+++ Matrix/python/odasOffline.py
@@ -107,7 +107,9 @@ def gen_config(
             idx = np.where([(keyword in ll.split("/")[-1]) for ll in lines])[0]
         elif keyword in ["separated", "postfiltered"]:
             idx = np.where([(keyword+".raw" in ll.split("/")[-1]) for ll in lines])[0]
-            newpath = "".join([target_path , "/recordings", array_idx , "/", keyword , "/" , keyword , "_" , tag , ".raw"])
+            # newpath = "".join([target_path , "/recordings", array_idx , "/", keyword , "/" , keyword , "_" , tag , ".raw"])
+            print('gen_config : use dummypath for', keyword) # to save disk space
+            newpath = "".join([target_path , "/recordings", array_idx , "/", keyword , "/" , keyword , "_dummy.raw"])
 
         for ii in idx: # replace path in matching lines
             line = lines[ii].split("\"")
